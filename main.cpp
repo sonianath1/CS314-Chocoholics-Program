91f8634d47888a40a61094136258b988679766fb
Sonia Nath <nathson@pdx.edu>
2025-10-27 18:40:27 -0700

    simple start of main cpp file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+Chocoholics CS314 Program
+Names: Sonia Nath,
+Description of Program: 
+*********************************************************************************/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main(void)
+{
+	
+
+
+
+
+	return 0;
+}
