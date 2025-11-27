b7f6f18c6e91de59b948ef04a03c44d5556d9f43
olga ishimwe <oishimwe@pdx.edu>
2025-11-26 21:37:35 -0800

    progress

--- database.cpp
+++ database.cpp
@@ -1,4 +1,6 @@
 #include "database.h"
+#include <iostream>
+using namespace std
 /*******************************************************************************
 Chocoholics CS314 Program
 Names: Sonia Nath, Yollaine Brooks,
@@ -64,7 +66,102 @@ void Database::get_service_data(int service_number, service& copy)
 //olga
 void Database::generate_weekly_report()
 {
+    //seraches thorough the memebrs hash table
+    Member* find_memeber(int num)
+    {
+        node<Member>* current = Members[i];
+        while(current != NULL)
+        {
+            if(current->data.get_number() == num)
+                return &current->data;
+            current = current->next;
+        }
+    }
+    return NULL;
 
+    //FINDS TE PROVIDER BY NUMBER
+    Provider* find_provider(int num)
+    {
+        node<Provider>* current = Providers[i];
+        while(current != NULL)
+        {
+            if(current->data.get_number() == num)
+                return &current->data;
+            current = current->next;
+        }
+    }
+    return NULL;
+
+    //FIND serivce using code
+    Service* find_service(int code);
+    {
+        for(int i = 0; i < prov_dir_size; i++)
+        {
+            node<SERVICE>* current = ProviderDirectory[i];
+            while(current != NULL)
+            {
+                if(current->data.code == code)
+                    return &current->data;
+                current = current->next;
+            }
+        }
+        return NULL;
+    }
+    
+    //output file
+    ofstream report("Weekly_report.txt")
+    if(!report)
+    {
+        cout<<"Error:Couldn't create weekly_report.txt\n";
+        return;
+    }
+
+    report<<"********** ChpcAn Weekly Report **********\n\n";
+
+    //keep wit the total services we provides and total amount
+    int total_services = 0;
+    double total_fees = 0.0;
+    
+    vector<int> unique_members;
+
+    vector<int> provider_ids;
+    vector<double> provider_amounts;
+
+    for(int i = 0; i < ProvidedServices.size(); i++)
+    {
+        ProvidedService ps = ProvidedServices[i];
+
+        int member_num = ps.member_number;
+        int provider_num = ps.provider_number;
+        int service_code = ps.service_code;
+
+        Member* mem = find_memebr(member_num);
+        string memeber_name = "Unknown";
+        if(mem != NULL)
+            member_name = m->get_name();
+
+        Provider* prov = find_provider(provide_num);
+        string provider_name = "Unknown";
+        if(prov != NULL)
+            provider_name = prov->get_name();
+
+        Service* serv = find_service(service_code);
+        string service_name = "Unknown";
+        double fee = 0.0;
+        if(serv != NULL)
+        {
+            service_name = serv->name;
+            fee = serv->fee;
+        }
+
+        //wrtinr into the report
+        report <<"Service Date: "<<ps.service_data_time<<endl;
+        report<<"Member: "<<memebr_num<<"-"<< member_name <<endl;
+        report<<"Provider: "<<provider_num <<"-"<<provider_name<<endl;
+        report<<"Service: "<<service_name<<endl;
+        report<<"Fee: "<<fee<<" $ "<<endl;
+        report<<"---------------------------------------------------------"<<endl;
+    }
 }
 
 void Database::verify_member(int member_number)
