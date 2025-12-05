e15a0ef0b8ddf6504b53de80ff71ca4034327e84
noodle-strudel <120442383+noodle-strudel@users.noreply.github.com>
2025-12-04 17:03:45 -0800

    Update test_main.cpp
    fixed test_main.cpp, had old stuff in it somehow

--- test_main.cpp
+++ test_main.cpp
@@ -4,39 +4,6 @@
 int main()
 
 {    
-    DatabaseTest test_db;
-    ServiceTest test_service;
-    
-    cout << "SMOKE TESTING\n" 
-         << "-------------\n" << endl;
-    test_db.constructor_test();
-    test_db.read_from_file_test();
-    test_db.write_to_file_test();
-    test_db.add_member_test();
-    test_db.update_member_test();
-    test_db.delete_member_test();
-    test_db.add_provider_test();
-    test_db.update_provider_test();
-    test_db.delete_provider_test();
-    
-    
-    cout << "SERVICE TESTING\n" 
-         << "-------------\n" << endl;
-    test_service.default_constructor_test();
-    test_service.constructor_test();
-    test_service.get_number_test();
-    test_service.get_name_test();
-    test_service.get_fee_test();
-    test_service.set_name_test();
-    test_service.set_code_test();
-    test_service.set_fee_test();
-    test_service.display_all_test();
-    
-
-    
-    return 0;
-
-{
      DatabaseTest test_db;
      ServiceTest test_service;
      EntityTest test_entity;
